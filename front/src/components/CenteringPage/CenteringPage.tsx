e30a4653762bb82c08ff46eeb94c606ee9ca22d9
Fitiavana Ramanandafy <fitiavana.ramanandafy@gmail.com>
2021-09-20 01:19:51 +0300

    change login/signup background

--- front/src/components/CenteringPage/CenteringPage.tsx
+++ front/src/components/CenteringPage/CenteringPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CenteringPage: React.FC = ({ children }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen px-4 py-12 bg-blue-100 sm:px-6 lg:px-8">
+    <div className="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-100 sm:px-6 lg:px-8">
       {children}
     </div>
   )
